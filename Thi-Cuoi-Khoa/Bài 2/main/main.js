f9edd3d12ec42d78e6e2ac63112c722e21688ae4
Nekiiii12 <128503452+Nekiiii12@users.noreply.github.com>
2023-03-21 22:45:10 +0700

    Add files via upload

---
+++ Thi-Cuoi-Khoa/Bài 2/main/main.js
@@ -0,0 +1,13 @@
+function numberOneTriangle() {
+    let number = document.getElementById('number').value;
+    let emtyPlace = '';
+    const resultDiv = document.getElementById('result');
+    resultDiv.innerHTML = '';
+    for (let i = 0; i < number; i++) {
+        emtyPlace += '*';
+        let newDiv = document.createElement('div');
+        newDiv.innerHTML = emtyPlace;
+        resultDiv.appendChild(newDiv);
+    }
+}
+numberOneTriangle();
